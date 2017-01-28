e28cf805a94343b361254847b876e4e06a4be894
msamoylenko <ghost1212@mail.ru>
2017-01-28 22:38:04 +0500

    Implemented basic either type.

---
+++ ImplicitEither/Either.cs
@@ -0,0 +1,27 @@
+﻿namespace ImplicitEither
+{
+    public class Either<L, R>
+    {
+        public L Left { get; }
+        public R Right { get; }
+        public bool IsLeft { get; }
+        public bool IsRight => !IsLeft;
+
+        private Either(L left, R right, bool isLeft)
+        {
+            Left = left;
+            Right = right;
+            IsLeft = isLeft;
+        }
+
+        public static Either<L, R> Create(L left)
+        {
+            return new Either<L, R>(left, default(R), isLeft: true);
+        }
+
+        public static Either<L, R> Create(R right)
+        {
+            return new Either<L, R>(default(L), right, isLeft: false);
+        }
+    }
+}
