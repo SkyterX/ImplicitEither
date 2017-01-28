162d8140c00d9833fbf3c0b62aec1533c8fc367b
msamoylenko <ghost1212@mail.ru>
2017-01-28 22:39:39 +0500

    Added type artificial type constraint to restrict usage of either with same type parameters.

---
+++ ImplicitEither.Tests/TypeConstraints_Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ImplicitEither.Tests
+{
+    [TestFixture]
+    public class TypeConstraints_Tests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.Throws<TypeInitializationException>(TryCreateEither<string, string>());
+            Assert.DoesNotThrow(TryCreateEither<string, int>());
+        }
+
+        private static TestDelegate TryCreateEither<TL, TR>()
+        {
+            return () =>
+            {
+                Either<TL, TR>.Create(default(TL));
+                Either<TL, TR>.Create(default(TR));
+            };
+        }
+    }
+}
