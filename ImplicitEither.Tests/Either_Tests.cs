a49f513d6beb575657b07cc4fc75d15d7720c51f
msamoylenko <ghost1212@mail.ru>
2017-01-28 23:46:58 +0500

    Basic operations on Either type

---
+++ ImplicitEither.Tests/Either_Tests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+
+namespace ImplicitEither.Tests
+{
+    [TestFixture]
+    public class Either_Tests
+    {
+        private const string stringValue = "0";
+        private const int intValue = 0;
+
+        private Either<int, string> EitherInt { get; } = Either<int, string>.Create(intValue);
+        private Either<int, string> EitherString { get; } = Either<int, string>.Create(stringValue);
+
+
+        [Test]
+        public void TestGetLeft()
+        {
+            AssertInt(EitherInt.Left());
+            Assert.Catch(() => EitherString.Left());
+            AssertInt(EitherString.Left(right: int.Parse));
+        }
+
+        [Test]
+        public void TestGetRight()
+        {
+            AssertString(EitherString.Right());
+            Assert.Catch(() => EitherInt.Right());
+            AssertString(EitherInt.Right(left: l => l.ToString()));
+        }
+
+        [Test]
+        public void TestReturn()
+        {
+            AssertInt(EitherString.Return(left: l => l, right: int.Parse));
+            AssertString(EitherInt.Return(left: l => l.ToString(), right: r => r));
+        }
+
+        [Test]
+        public void TestDo()
+        {
+            EitherInt.Do(left: AssertInt, right: r => Assert.Fail());
+            EitherString.Do(left: l => Assert.Fail(), right: AssertString);
+        }
+
+        private void AssertInt(int x) => Assert.That(x, Is.EqualTo(intValue));
+        private void AssertString(string x) => Assert.That(x, Is.EqualTo(stringValue));
+    }
+}
--- ImplicitEither/Either.cs
+++ ImplicitEither/Either.cs
@@ -1,11 +1,20 @@
-﻿namespace ImplicitEither
+﻿using System;
+
+namespace ImplicitEither
 {
     public class Either<L, R>
     {
-        public L Left { get; }
-        public R Right { get; }
-        public bool IsLeft { get; }
-        public bool IsRight => !IsLeft;
+        static Either()
+        {
+            if (typeof(L) == typeof(R))
+                throw new Exception(
+                    $"Using the type Either with equal type parameters ({typeof(L).Name}) is prohibited.");
+        }
+
+        internal L Left { get; }
+        internal R Right { get; }
+        internal bool IsLeft { get; }
+        internal bool IsRight => !IsLeft;
 
         private Either(L left, R right, bool isLeft)
         {
@@ -22,6 +31,40 @@
         public static Either<L, R> Create(R right)
         {
             return new Either<L, R>(default(L), right, isLeft: false);
+        }
+    }
+
+    public static class EitherExtensions
+    {
+        public static L Left<L, R>(this Either<L, R> either, Func<R, L> right = null)
+        {
+            if (either.IsLeft)
+                return either.Left;
+            if (right != null)
+                return right(either.Right);
+            throw new ArgumentException($"Either is {typeof(R).Name}, but no suitable conversion to {typeof(L).Name} provided.", nameof(either));
+        }
+
+        public static R Right<L, R>(this Either<L, R> either, Func<L, R> left = null)
+        {
+            if (either.IsRight)
+                return either.Right;
+            if (left != null)
+                return left(either.Left);
+            throw new ArgumentException($"Either is {typeof(L).Name}, but no suitable conversion to {typeof(R).Name} provided.", nameof(either));
+        }
+
+        public static TResult Return<L, R, TResult>(this Either<L, R> either, Func<L, TResult> left, Func<R, TResult> right)
+        {
+            return either.IsLeft ? left(either.Left) : right(either.Right);
+        }
+
+        public static void Do<L, R>(this Either<L, R> either, Action<L> left = null, Action<R> right = null)
+        {
+            if (left != null && either.IsLeft)
+                left(either.Left);
+            if (right != null && either.IsRight)
+                right(either.Right);
         }
     }
 }
